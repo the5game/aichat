a8c2f61ad4abf09e8a6984525aa435247f52d856
the5game <82686239+the5game@users.noreply.github.com>
2025-03-21 10:35:47 +0100

    Update login.js

--- login.js
+++ login.js
@@ -9,6 +9,10 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const storedPassword = getCookie('password');
 
     if (username === storedUsername && password === storedPassword) {
+        // Stocker le status de connexion et le nom d'utilisateur actif
+        setCookie('loggedIn', 'true', 7); // Stocker pour 7 jours
+        setCookie('activeUser', username, 7); // Stocker le nom d'utilisateur actif
+        
         alert("Connexion réussie!");
         // Rediriger vers la page principale
         window.location.href = "index.html";
@@ -21,4 +25,14 @@ function getCookie(name) {
     let value = "; " + document.cookie;
     let parts = value.split("; " + name + "=");
     if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
