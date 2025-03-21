be21d961635938c134194ae0aee0822d9332d08d
the5game <82686239+the5game@users.noreply.github.com>
2025-03-21 09:20:37 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// config.js
+const API_KEY = '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';
