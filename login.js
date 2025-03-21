f5388b13bc0860f3323ef1fc984e20066addab94
the5game <82686239+the5game@users.noreply.github.com>
2025-03-21 08:51:08 +0100

    Update login.js

--- login.js
+++ login.js
@@ -4,22 +4,6 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Simuler une vérification de connexion
-    if (username === "admin" && password === "password") {
-        alert("Connexion réussie!");
-        // Rediriger vers la page principale
-        window.location.href = "index.html";
-    } else {
-        alert("Nom d'utilisateur ou mot de passe incorrect.");
-    }
-});
-
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
     // Vérifier les informations de connexion avec les cookies
     const storedUsername = getCookie('username');
     const storedPassword = getCookie('password');
